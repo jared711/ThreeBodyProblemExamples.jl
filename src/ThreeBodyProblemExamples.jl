de6f1c55f28bd564f1d1c92a1a1056c027dfcd51
Jared Blanchard <jared711@stanford.edu>
2023-02-06 13:19:26 -0800

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.30

---
+++ src/ThreeBodyProblemExamples.jl
@@ -0,0 +1,5 @@
+module ThreeBodyProblemExamples
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using ThreeBodyProblemExamples
+using Test
+
+@testset "ThreeBodyProblemExamples.jl" begin
+    # Write your tests here.
+end
