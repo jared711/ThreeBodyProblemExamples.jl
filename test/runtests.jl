90c9d072577151c821e1508cee89c49a32581ceb
Jared Blanchard <jared711@stanford.edu>
2023-02-17 01:55:50 -0800

    Delete runtests.jl

--- test/runtests.jl
+++
@@ -1,9 +0,0 @@
-# using ThreeBodyProblemExamples
-using Test
-
-@testset "ThreeBodyProblemExamples.jl" begin
-    @test !isempty(include("../01_SystemDefinition.jl"))
-    @test !isempty(include("../02_Trajectories.jl"))
-    @test !isempty(include("../03_ForbiddenRegion.jl"))
-    @test !isempty(include("../08_QuasiPeriodicOrbits.jl"))
-end
